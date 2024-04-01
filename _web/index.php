540417f90210085f513c8ea8adb3210d1d43b6a5
justinojjsj <justinojjsj@gmail.com>
2024-04-01 13:30:05 -0300

    Update

--- _web/index.php
+++ _web/index.php
@@ -8,8 +8,8 @@
 
             <div class="collapse" id="navbarToggleExternalContent">
                 <div class="bg-dark p-4">
-                    <h5 class="text-white h4">Collapsed content</h5>
-                    <span class="text-muted">Toggleable via the navbar brand.</span>
+                    <h5 class="text-white h4">Sanja Hoje</h5>
+                    <span class="text-muted">Sistema de Micro Serviços de Mobilidade para São José dos Campos - Projeto Integrador III - UNIVESP</span>
                 </div>
             </div>
             <nav class="navbar navbar-dark bg-dark">
@@ -27,8 +27,14 @@
 
             <br>    
 
+            
+            <div class="alert alert-info text-center rounded-pill" role="alert">
+                <h2>
+                São José dos Campos - SP
+                </h2>
+            </div>
             <div id='telaSmart'>
-                <div id='relogioSmart'>
+                <div id='relogioSmart'>                    
                     <span id='hSmart'></span><span class='doisPontos'>:</span><span id='mSmart'></span><span class='doisPontos'>:</span><span id='sSmart'></span>
                 </div>
                 <div id='dataSmart'>
@@ -45,11 +51,11 @@
                         <div class="card h-100">
                         <img src="./file.jpg" class="card-img-top" alt="...">
                         <div class="card-body">
-                            <h5 class="card-title">Card title</h5>
-                            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                            <h5 class="card-title">Condições Climáticas</h5>
+                            <p class="card-text">Dados obtidos do Centro de Previsão de Tempo e Estudos Climáticos do Instituto Nacional de Pesquisas Espaciais (CPTEC/INPE) </p>
                         </div>
                         <div class="card-footer">
-                            <small class="text-muted">Last updated 3 mins ago</small>
+                            <small class="text-muted">Última atualização 3 mins atrás</small>
                         </div>
                         </div>
                     </div>
@@ -58,8 +64,8 @@
                         <div class="card h-100">
                         <img src="./file.jpg" class="card-img-top" alt="...">
                         <div class="card-body">
-                            <h5 class="card-title">Card title</h5>
-                            <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+                            <h5 class="card-title">Alertas da Defesa Civil</h5>
+                            <p class="card-text">Informações obtidas da Defesa Civil do Estado de São Paulo, referente ao CEP XX.XXX-XXX (Rua Tal, Bairro Tal, SJC-SP).</p>
                         </div>
                         <div class="card-footer">
                             <small class="text-muted">Last updated 3 mins ago</small>
@@ -71,8 +77,8 @@
                         <div class="card h-100">
                         <img src="./file.jpg" class="card-img-top" alt="...">
                         <div class="card-body">
-                            <h5 class="card-title">Card title</h5>
-                            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+                            <h5 class="card-title">Condições de Tráfego na Via Dutra</h5>
+                            <p class="card-text">Dados Obtidos da Concessionáio CCR-RIOSP, referente ao trecho de São José dos Campos, Km XXX ao KM XXX.</p>
                         </div>
                         <div class="card-footer">
                             <small class="text-muted">Last updated 3 mins ago</small>
