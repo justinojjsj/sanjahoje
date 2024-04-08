198831807d0cb608d2ff58ffce5ab0c7bf5742e0
Joel Justino <justinojjsj@gmail.com>
2024-04-08 10:30:16 -0300

    inpe.py finalizado

--- _app/inpe_bkp.py
+++ _app/inpe_bkp.py
@@ -2,7 +2,7 @@
 #crontab -e
 #0,15,30,45 * * * * /app/exec_inpe.sh
 
-#1. Entrar no site https://tempo.cptec.inpe.br/sp/sao-jose-dos-campos
+#Importando bibliotecas necessárias
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
@@ -10,27 +10,18 @@ from selenium.webdriver.firefox.options import Options
 from datetime import date
 from datetime import datetime
 import mysql.connector
-
 db_connection = mysql.connector.connect(host='170.14.0.3', user='root', password='my-secret-pw', database='db_inpe')
-  
+
+#Copia os dados do dia de hoje  
 f_options=Options()
-f_options.add_argument("-headless")
-  
+f_options.add_argument("-headless")  
 options = webdriver.FirefoxOptions()  
 webBrowser = webdriver.Firefox(options=f_options)
 webBrowser.get('https://tempo.cptec.inpe.br/sp/sao-jose-dos-campos')
-
-#2. Copiar os dados do dia de hoje
-
-#texto = webBrowser.find_element(By.XPATH, "/html/body").text
 texto = webBrowser.find_element(By.CLASS_NAME, "previsao").text
 webBrowser.close()
-
 texto = texto.splitlines()
-
-#print(texto)
 tamanho = len(texto)
-#print(tamanho)
 
 #Coleta das últimas informações do Banco
 consulta = db_connection.cursor()
@@ -46,20 +37,16 @@ db_ind_uv = db_dados[7]
 db_amanhecer = db_dados[8]
 db_entardecer = db_dados[9]
 
-#print(data_banco)
-
-#Coleta a data atual
+#Coleta a data e hora atuais
 data_hoje = date.today()
 dataFormatada = data_hoje.strftime('%d/%m/%Y')
-
-#Coleta a hora que as informações estão sendo coletadas
 hora = datetime.now()
 hora = hora.strftime('%H:%M:%S')
 
-data = texto[1]
-
-if (db_data!=dataFormatada):#If para verificar se já foi salvo os dados do banco hoje
-
+#Função que organiza informações coletadas do site e insere no banco
+def coleta():
+    data = texto[1]
+    
     if (tamanho == 13):#Tem dias que o INPE não divulga a chuva de manhã
         chuva_manha = 'Sem dados'
         chuva_tarde = texto[3]
@@ -100,8 +87,11 @@ if (db_data!=dataFormatada):#If para verificar se já foi salvo os dados do banc
     db_connection.commit()
     db_connection.close()
 
+if (db_data!=dataFormatada):#If para verificar se já foi salvo os dados do banco hoje
+    coleta()
 else:
     if(db_chuva_manha != texto[3] or db_chuva_tarde != texto[5] or db_chuva_noite != texto[7] or db_temp_max != texto[9] or db_temp_min != texto[10] or db_ind_uv != texto[12] or db_amanhecer != texto[13] or db_entardecer != texto[14]):
         print("Dados já salvos no banco, mas houve atualização")
+        coleta()
     else:
         print("Dados já salvos no banco, NÃO houve atualização")
