2e6d8cce0a30beb2cd7deffd6b04810a840e1d2d
Joel Justino <joelsaudesucesso@gmail.com>
2024-05-13 19:24:10 -0300

    ccr quebrado

--- _app-ccr/ccr.py
+++ _app-ccr/ccr.py
@@ -15,15 +15,23 @@ f_options.add_argument("-headless")
 options = webdriver.FirefoxOptions()  
 webBrowser = webdriver.Firefox(options=f_options)
 webBrowser.get('https://rodovias.grupoccr.com.br/riosp/cameras-ao-vivo/?openModalCamera=open&camera=km-156-sp')
-time.sleep(5)
-texto = webBrowser.find_elements(By.CLASS_NAME, "cmp-modalCamera__alertCard__description")
+time.sleep(10)
+#texto = webBrowser.find_elements(By.CLASS_NAME, 'cmp-modalCamera__alertCard__description__title')
+
+texto = webBrowser.find_element(By.CLASS_NAME, "cmp-modalCamera__alertCard__description__title").text
 webBrowser.close()
 #texto = texto.splitlines()
 #tamanho = len(texto)
 
+print(texto)
+
 #print(texto[1].text)
-for item in texto:
-    titulo = item.find_element(By.CLASS_NAME, "cmp-modalCamera__alertCard__description__title").text
-    print(titulo)
+#time.sleep(10)
+
+#for item in texto:
+    #titulo = item.find_element(By.CLASS_NAME, "cmp-modalCamera__alertCard__description__title").text
+    #print(titulo)
+    #time.sleep(5)
+    #print(item)
 
 #https://medium.com/@dev.daniel.amorim/selenium-web-scraping-parte-iii-4478a07e0afa
